f1c5d18586be3ddec26868198dffe9acf961282d
arthur <fatih@qq.com>
2021-06-04 11:53:41 +0800

    bingo, template finished

--- src/test.cpp
+++ src/test.cpp
@@ -16,15 +16,13 @@ int main(int argc, char * argv[]){
 
 
 	heapTimer.start_timer(2, handle_timeout); 
-
-//	heapTimer.start_timer(3, [](HeapTimer<>::TimerNodePtr node ){
-//			auto unode = node->get_user_node<UserTimerNode<int>  > (); 
-//			std::cout << "handle timeout 111 " << std::time(0) << std::endl ; 
-//			//std::cout << "user data is  " << unode->user_data << std::endl ; 
-//			return true; 
-//
-//			}, true , 3333); 
-
+	heapTimer.start_timer(3, [](HeapTimer<>::TimerNodePtr node) {
+		   
+		
+		std::cout << "handle timeout 333 " << std::time(0) << std::endl; 
+			
+		return true; //don't forget to return,   false will stop the timer 
+			}	); 
 
 	heapTimer.start(); 
 
